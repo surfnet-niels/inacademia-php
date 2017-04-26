9373e8af8cb062c9d2bc2a0e4c5856d09184e7ee
Yiannis Kakavas <ikakavas@noc.grnet.gr>
2017-04-26 17:42:32 +0300

    Fixed error message for supported affiliations

--- inacademia-php/inacademia.php
+++ inacademia-php/inacademia.php
@@ -11,7 +11,7 @@ $InAcademiaURL = 'https://op.srv.inacademia.org';
 
 function validate($clientID, $requestedValidation, $redirectURL) {
     if (!in_array($requestedValidation, array('affiliated', 'faculty+staff', 'employee', 'student'), true)){
-        throw new Exception('Selected affiliation is not supported for handle_validation_resonse');
+        throw new Exception('Selected affiliation is not supported for validation');
     }
 	
 	$_SESSION['inacademia']['validated'] = false;
